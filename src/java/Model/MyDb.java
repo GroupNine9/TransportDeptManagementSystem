47a8f5fe59d508a7428eebd3ac7c80bea83b815f
Carol <>
2018-08-02 08:45:30 +0300

    Project Structure

--- src/java/Model/MyDb.java
+++ src/java/Model/MyDb.java
@@ -3,13 +3,11 @@ package Model;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class MyDb {
     Connection con;
     String driverName = "com.mysql.jdbc.Driver";
-    String connectionURL = "jdbc:mysql://localhost:3306/users?";
+    String connectionURL = "jdbc:mysql://localhost:3306/transportmanagement?";
     String username = "root";
     String password = "";
     
