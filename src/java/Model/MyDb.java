e3a94d2774c53232b470feb2b57b47b80e002f59
TinaKareh <gwanjiru35@gmail.com>
2018-08-10 10:10:02 -0700

    Initial Commit

--- src/java/Model/MyDb.java
+++ src/java/Model/MyDb.java
@@ -7,21 +7,18 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class MyDb {
+
     Connection con;
     String driverName = "com.mysql.jdbc.Driver";
     String connectionURL = "jdbc:mysql://localhost:3306/users?";
     String username = "root";
     String password = "";
-    
-    public Connection getCon()
-    {
-        try
-        {
+
+    public Connection getCon() {
+        try {
             Class.forName(driverName);
-            con = DriverManager.getConnection(connectionURL,username, password);
-        }
-        catch (ClassNotFoundException | SQLException e) 
-        {
+            con = DriverManager.getConnection(connectionURL, username, password);
+        } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
         return con;
